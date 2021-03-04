f1e0926c28a9725658a069d866077c87abbda038
Coolbrother <coolbrother@sourcegod.com>
2021-03-04 11:51:47 +0100

    Added file (src/main.cpp)

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+/*
+ * file: main.cpp
+ * Main file
+  * Compile: 
+ * g++ -std=c++11 -Wall -pedantic myfile.cpp -o myfile
+ * Date: Tue, 02/03/2021
+ * Author: Coolbrother
+*/
+
+#include <iostream> // stdin, stdout
+#include <string> // string
+
+using namespace std;
+//----------------------------------------------------------
+//========================================
+
+void test() {
+    cout << "Test\n\n";
+
+    cout << endl;
+}
+//----------------------------------------------------------
+
+int main() {
+    test();
+  cout << "Finished...\n" << endl;
+
+  return 0;
+}
+//----------------------------------------------------------
